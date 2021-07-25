8ce7748ae7bc4ac8856f246e21a5ab121cc2c63a
Oscar <oscar.miras@gmail.com>
2021-07-25 08:33:23 +0200

    header

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+
+  ];
+
+  constructor() { 
+    this.recipes.push(new Recipe('A Test Recipe', 'Simply a Test', 'https://www.adobe.com/express/create/media_101046379bb5a3c0a3366cc3b0170c9e8244f753d.png'));
+  }
+
+  ngOnInit(): void {
+  }
+
+}
